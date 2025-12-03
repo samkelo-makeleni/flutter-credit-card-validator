8d7f65cc8a1e5a91802db4477accb741201ec8e1
Samkelo Makeleni <makeles1@telkom.co.za>
2025-12-03 19:17:50 +0200

    UI: add SettingsView navigation to HomeView

--- lib/pages/home_view.dart
+++ lib/pages/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'credit_card_form_view.dart';
 import 'saved_cards_view.dart';
+import 'settings_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -8,7 +9,17 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: const Text('Credit Card Validator'))),
+      appBar: AppBar(
+        title: const Center(child: Text('Credit Card Validator')),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => Navigator.of(context).push(
+              MaterialPageRoute(builder: (_) => const SettingsView()),
+            ),
+          ),
+        ],
+      ),
       body: const Padding(
         padding: EdgeInsets.all(12.0),
         child: Column(
